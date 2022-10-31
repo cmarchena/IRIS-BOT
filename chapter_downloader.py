f3f07c9421f8f5d3d49046f1613786929db85a96
Carlos Marchena <sprintwithcarlos@pm.me>
2022-10-31 17:18:58 +0100

    Closes Replace BASE URL #4

--- chapter_downloader.py
+++ chapter_downloader.py
@@ -1,42 +1,44 @@
 from storefront_downloader import decode_protobin, download_file
 import os
-import sys
-import time
 import re
-import json
-from filelocations import SCRIPTS as SCRIPTS
+from filelocations import SCRIPTS as SCRIPTS, BASE_URL
 
-#open storefront-dd-mm-yyyy.proto and get all links inside it
+
+# open storefront-dd-mm-yyyy.proto and get all links inside it
 def get_links(file_name):
     with open(file_name, "r") as file:
         links = []
         text = file.read()
-        match = re.search(r"https:\/\/choices-live.pixelberrystudios.com\/.*\.protobin", text)
-        #append match to links
+        match = re.search(
+            r"https:\/\/choices-live.pixelberrystudios.com\/.*\.protobin", text)
+        # append match to links
         while match:
             links.append(match.group(0))
             text = text[match.end():]
-            match = re.search(r"https:\/\/choices-live.pixelberrystudios.com\/.*\.protobin", text)
+            match = re.search(
+                r"https:\/\/choices-live.pixelberrystudios.com\/.*\.protobin", text)
         print(len(links))
         return links
 
+
 def download_new_chapters():
-    download_file("https://choices-live.pixelberrystudios.com/storefront-2.4.protobin", "storefront-2.4.protobin")
+    download_file(BASE_URL + "storefront-2.4.protobin",
+                  "storefront-2.4.protobin")
     decode_protobin("storefront-2.4.protobin")
     links = get_links("storefront-2.4.proto.txt")
     downloaded = []
-    #what is the current directory name?
+    # what is the current directory name?
     current_dir = os.getcwd().split("/")[-1]
     print("Current directory: ", current_dir)
     if os.path.exists("scripts"):
         os.chdir("scripts")
     for link in links:
-        #create folders according to link
+        # create folders according to link
         folder_name = link.split("/")[-2]
         file_name = link.split("/")[-1].split(".")[0]
         if not os.path.exists(folder_name):
             os.system("mkdir " + folder_name)
-        #download protobin file from link
+        # download protobin file from link
         if os.path.exists(folder_name + "/" + file_name + ".proto.txt"):
             continue
         else:
@@ -48,19 +50,22 @@ def download_new_chapters():
     print("Done!")
     return downloaded
 
-#function to list all files in a directory including subdirectories
+# function to list all files in a directory including subdirectories
+
+
 def list_files(dir):
     files = []
     print("current directory: ", dir)
-    #return all folders in dir
+    # return all folders in dir
     folders = [f.path for f in os.scandir(dir) if f.is_dir()]
     for root, dirs, filenames in os.walk(dir):
         for f in filenames:
             file_path = os.path.join(root, f)
-            #append the last part of the path to the list
+            # append the last part of the path to the list
             file_name = file_path.split("/")[-1]
             files.append(file_name[:-10])
     return sorted(folders), sorted(files)
+
 
 if __name__ == "__main__":
     print(download_new_chapters())
