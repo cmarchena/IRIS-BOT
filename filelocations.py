131ca3aa7412247bd710358ed0d741e697486990
Priya Sharma <77352100+DevPriSha@users.noreply.github.com>
2022-10-31 17:01:44 +0530

    Add filepaths and tokens

---
+++ filelocations.py
@@ -0,0 +1,9 @@
+#folder where iris.py exists (absolute filepaths only)
+ROOT = "/Users/devprisha/Documents/GitHub/iris-bot/"
+
+#DO NOT TOUCH THESE
+SCRIPTS = ROOT + "scripts"
+ASSETS = ROOT + "assets"
+
+#replace with your discord token
+TOKEN = "Your discord bot token"
